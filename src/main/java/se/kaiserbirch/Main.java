dc525e211775ab67a7fff36e3cdb0efee3c3e67a
Franz - Josef Björck <fj.bjorck@protonmail.com>
2023-11-03 20:17:00 +0100

    chore: cleaned up main after testing.

--- src/main/java/se/kaiserbirch/Main.java
+++ src/main/java/se/kaiserbirch/Main.java
@@ -4,17 +4,9 @@ import se.kaiserbirch.controller.Controller;
 import se.kaiserbirch.model.ModelController;
 import se.kaiserbirch.view.ViewController;
 
+
 public class Main {
     public static void main(String[] args) {
-        /*
-        controller.addProducer();
-        controller.addProducer();
-        controller.addProducer();
-        controller.addProducer();
-        controller.addProducer();
-        controller.addProducer();
-        controller.addProducer();
-         */
         Controller controller = new Controller(new ModelController());
         ViewController viewController = new ViewController(controller);
         viewController.init();
